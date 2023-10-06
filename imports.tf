c4a976974d5e2a6eed10740deb9b659822e0afb0
Rafael Cysneiros <cys.rafael@gmail.com>
2023-10-06 16:51:55 -0300

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0d85bdbce0a7cc5cd" #VPC
+# }
+# 
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-0f474f69e6f53b950" #PublicSubnet1
+# }
+# 
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-0c8120fc4633a78d5" #PublicSubnet2
+# }
+# 
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-00336c973de65dc25" #InternetGateway
+# }
+# 
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0c9055e1ef02b92ee_0.0.0.0/0" #DefaultPublicRoute
+# }
+# 
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0c9055e1ef02b92ee" #PublicRouteTable
+# }
+# 
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-0f474f69e6f53b950/rtb-0c9055e1ef02b92ee" #PublicSubnet1/PublicRouteTable
+# }
+# 
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-0c8120fc4633a78d5/rtb-0c9055e1ef02b92ee" #PublicSubnet2/PublicRouteTable
+# }
+# 
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-03d5e8060bd1952b4" #NoIngressSecurityGroup
+# }
